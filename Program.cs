75fb3510c0752289f1d5c945131ebef027467937
masum <md.wr.masum@gmail.com>
2022-12-29 23:30:08 +0600

    Create database and container

--- Program.cs
+++ Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Azure.Cosmos;
+
+namespace AzureCosmos;
+
+public class Program
+{
+    // Replace <documentEndpoint> with the information created earlier
+    private static readonly string EndpointUri = "<documentEndpoint>";
+
+    // Set variable to the Primary Key from earlier.
+    private static readonly string PrimaryKey = "<your primary key>";
+
+    // The Cosmos client instance
+    private CosmosClient? _cosmosClient;
+
+    // The database we will create
+    private Database? _database;
+
+    // The container we will create.
+    private Container? _container;
+
+    // The names of the database and container we will create
+    private string databaseId = "az204Database";
+    private string containerId = "az204Container";
+
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Beginning operations...\n");
+            Program p = new Program();
+            await p.CosmosAsync();
+
+        }
+        catch (CosmosException de)
+        {
+            Exception baseException = de.GetBaseException();
+            Console.WriteLine("{0} error occurred: {1}", de.StatusCode, de);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: {0}", e);
+        }
+        finally
+        {
+            Console.WriteLine("End of program, press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    public async Task CosmosAsync()
+    {
+        // Create a new instance of the Cosmos Client
+        _cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+
+        // Runs the CreateDatabaseAsync method
+        await CreateDatabaseAsync();
+
+        // Run the CreateContainerAsync method
+        await CreateContainerAsync();
+    }
+
+    private async Task CreateDatabaseAsync()
+    {
+        // Create a new database using the cosmosClient
+        _database = await _cosmosClient!.CreateDatabaseIfNotExistsAsync(databaseId);
+        Console.WriteLine("Created Database: {0}\n", _database.Id);
+    }
+
+    private async Task CreateContainerAsync()
+    {
+        // Create a new container
+        _container = await _database!.CreateContainerIfNotExistsAsync(containerId, "/LastName");
+        Console.WriteLine("Created Container: {0}\n", _container.Id);
+    }
+}
